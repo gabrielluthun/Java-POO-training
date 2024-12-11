d40461909a9c9fe280dbf6e6f4c43db4d035ec3f
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-12-11 16:50:11 +0100

    feat(Bank.java): add bank class

---
+++ bank-exercise/Bank.java
@@ -0,0 +1,19 @@
+public class Bank {
+
+    private String name;
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public <T, S> void transfert(Account<T> sourceAccount, Account<S> targetAccount, int amount) {
+
+       if (sourceAccount.getAmount() >= amount) {
+           sourceAccount.removeMoney(amount);
+           targetAccount.addMoney(amount);
+           System.out.println("Transfert de " + amount + " " + sourceAccount.getCurrency() + " de " + sourceAccount.getOwner() + " à " + targetAccount.getOwner() + " effectué.");
+       } else {
+           System.out.println("Transfert impossible ! " + sourceAccount.getOwner() + " n'a pas assez d'argent sur son compte.");
+       }
+    }
+}
